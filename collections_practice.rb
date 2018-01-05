675045dc0885308fa7db62c8a5e8654022884ac2
Christina Collura <cmcollura@gmail.com>
2018-01-05 01:55:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,15 +1,11 @@
 def begins_with_r(array)
-  if block_given?
   i = 0 
   while i < array.length
-    if yield(array[i])
+    if array[i].starts_with?("r")
       true 
     else
       false 
     end
-  i += 1 
-  end
- else
-   puts "No block was given!"
- end 
+   i += 1
+ end
 end
